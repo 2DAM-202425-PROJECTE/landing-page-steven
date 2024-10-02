ee3318eb0033706a0e224866e4b1143e26e0e737
Steven <mromero1@iesebre.com>
2024-10-02 20:54:07 +0200

    responsive + seccions

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ Alpine.start()
 AOS.init({
     duration: 1000, // Duració de l'animació en milisegons
 });
+
+document.querySelectorAll('[data-toggle]').forEach(item => {
+    item.addEventListener('click', () => {
+        const content = item.nextElementSibling; // El següent element (la resposta)
+        const icon = item.querySelector('.toggle-icon'); // L'ícona + o -
+
+        if (content.classList.contains('hidden')) {
+            content.classList.remove('hidden'); // Mostrar resposta
+            icon.textContent = '-'; // Canviar símbol a -
+        } else {
+            content.classList.add('hidden'); // Ocultar resposta
+            icon.textContent = '+'; // Canviar símbol a +
+        }
+    });
+});
